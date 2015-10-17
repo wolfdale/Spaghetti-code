d6650475128e3db8396a46828d701c89b6da48a1
Prakhar Gupta <mywolfdale@gmail.com>
2015-10-17 18:41:02 +0530

    Create 2_or_3.c

---
+++ 2_or_3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void main()
+{
+    int i;
+ for(i=1;i<=100;i++)
+ {
+     if(i%2==0)
+     {
+        if( (i%2==0) && (i%3==0) )
+        {
+        
+            printf("RED BALLON\n");
+        
+        }
+         printf("RED\n");
+
+     }
+     else if(i%3==0)
+     {
+
+         printf("BALLON\n");
+     }
+    else
+     {
+         printf("%d\n",i);
+     }
+ }
+
+}
