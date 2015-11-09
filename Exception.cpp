0c71564286b7f10b458364c4f27bb0e5c2473635
Prakhar Gupta <mywolfdale@gmail.com>
2015-11-09 11:53:04 +0530

    Create Exception.cpp

---
+++ Exception.cpp
@@ -0,0 +1,28 @@
+//Throws Keyword Implementation
+#include <iostream>
+using namespace std;
+
+double division(int a, int b)
+{
+   if( b == 0 )
+   {
+      throw "Division by zero condition!";
+   }
+   return (a/b);
+}
+
+int main ()
+{
+   int x = 50;
+   int y = 0;
+   double z;
+ 
+   try {
+     z = division(x, y);
+     cout << z << endl;
+   }catch (const char* msg) {
+     cout << msg << endl;
+   }
+
+   return 0;
+}
