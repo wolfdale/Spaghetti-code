7e5c39e0225c9b23b422a69ef586f4dc4a0bd045
Prakhar Gupta <mywolfdale@gmail.com>
2015-12-22 21:05:42 +0530

    Switch Statement

---
+++ JavaProgram/src/SampleSwitch.java
@@ -0,0 +1,33 @@
+public class SampleSwitch {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++)
+		{
+			switch(i)
+			{
+			case 1:
+				System.out.println("i is 1");
+				break;
+			case 2:
+				System.out.println("i is 2");
+				break;
+			case 3:
+				System.out.println("i is 3");
+				break;
+			case 4:
+				System.out.println("i is 4");
+				break;
+			case 5:
+				System.out.println("i is 5");
+				break;
+			case 6:
+				System.out.println("This will not execute");
+				break;
+				
+			}
+		}
+	
+	}
+
+}
