f2a69de7eff2714cee9616953f049b471849900d
Prakhar Gupta <mywolfdale@gmail.com>
2015-12-22 20:23:36 +0530

    Java Programs

---
+++ JavaProgram/src/overload.java
@@ -0,0 +1,39 @@
+class area
+{
+	int a,b;
+	
+	void calarea(int a, int b)
+	{
+		System.out.println("Area of Rect.");
+		System.out.println(a*b);
+
+	}
+	
+	void calarea(int a)
+	{
+		System.out.println("Area of Circle");
+		System.out.println(3.14*a*a);
+		
+	}
+	
+	
+}
+
+
+
+
+
+
+public class overload {
+
+	public static void main(String[] args) {
+		
+		area something = new area();
+		
+		something.calarea(5);
+		
+		something.calarea(5 ,8);
+		
+	}
+
+}
