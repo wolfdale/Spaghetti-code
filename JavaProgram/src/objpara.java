7c949ca8c6abf77636f1b28655a500f5f8cd6b84
Prakhar Gupta <mywolfdale@gmail.com>
2015-12-27 19:39:42 +0530

    Changes

--- JavaProgram/src/objpara.java
+++ JavaProgram/src/objpara.java
@@ -8,7 +8,7 @@ class equal_test
 	}
 	boolean test_equality(equal_test o)
 	{
-		if(o.i == o.j)
+		if(o.i == i && j == o.j)
 		{
 			return true;
 		}
@@ -26,14 +26,13 @@ public class objpara {
 
 	public static void main(String[] args) {
 		
-		boolean result = false;
-		equal_test obj1 = new equal_test(5 ,6);
-		obj1.test_equality(obj1);
-		System.out.println(result);
 		
+		equal_test obj1 = new equal_test(5 ,8);
 		equal_test obj2 = new equal_test(5 ,5);
-		obj2.test_equality(obj2);
-		System.out.println(result);
+		obj1.test_equality(obj2);
+		System.out.println(obj1.test_equality(obj2));
+		
+		
 		
 	}
 
