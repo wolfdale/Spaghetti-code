8270175ff9fc5a808290622b66fc4e9b17fef59f
Prakhar Gupta <mywolfdale@gmail.com>
2016-01-09 17:28:00 +0530

    Strings

---
+++ JavaProgram/src/string_Demo.java
@@ -0,0 +1,17 @@
+
+public class string_Demo {
+
+	public static void main(String[] args) {
+		// Strings
+		
+		String name = "Hello World!";
+		
+		System.out.println(name);
+		
+		for(int i=0; i <name.length() ; i++)
+			{
+				System.out.println(name.charAt(i));
+			}
+	}
+
+}
