3e8753ca4e864ea5f43a2eb59c4ab85c8e0cdeeb
Prakhar Gupta <mywolfdale@gmail.com>
2016-01-11 22:50:52 +0530

    Redo

--- JavaProgram/src/String_Compare.java
+++ JavaProgram/src/String_Compare.java
@@ -5,11 +5,19 @@ public class String_Compare {
 		String s1 = new String("JAVA");
 		String s2 = new String("JAVA");
 		String s3 = new String("Java");
-		
+		String s4 = "Java";
 		System.out.println(s1.equals(s2)); //Print True
 		System.out.println(s2.equals(s3));//Prints false
 		System.out.println(s2.equalsIgnoreCase(s3)); //Print True Ignore case
 
+		if(s4 == s3)
+		{
+			System.out.println("This will Not print");
+		}
+		else
+		{
+			System.out.println("The = = operator compares references not values.");
+		}
 		
 		
 
