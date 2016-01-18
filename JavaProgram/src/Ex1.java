e5d1748344f758ac40b556edd7aa6978172b45fa
Prakhar Gupta <mywolfdale@gmail.com>
2016-01-18 19:31:53 +0530

    OCA

---
+++ JavaProgram/src/Ex1.java
@@ -0,0 +1,14 @@
+
+public class Ex1 {
+	static int i =10;
+	public static void main(String[] args) {
+	
+		for(int i=0;i<5;i++) //life of variable i ends when loop completes its execution
+		{
+			System.out.println(i);
+		}
+		System.out.println(i);
+
+	}
+
+}
