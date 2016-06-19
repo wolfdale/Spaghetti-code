d6123f4f0cf692c8cbbc4aa9c0e171553ecaef3e
Prakhar <mywolfdale@gmail.com>
2016-06-19 13:25:18 +0530

    Class in Python

--- Python/class_basic.py
+++ Python/class_basic.py
@@ -1,7 +1,9 @@
 #Implementing Simple class in Python
 
 class Point(object):
-	pass
+	def reset(self):
+		self.x = 0
+		self.y = 0
 
 p1 = Point()
 p1.x = 5
@@ -10,6 +12,9 @@ p1.y = 5
 print p1
 print p1.x
 print p1.y
-
+p1.reset()
+print "After Reset"
+print p1.x
+print p1.y
 
 
