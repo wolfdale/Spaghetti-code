a923f502cd0db0ef81bbed49c958f2407b91ea1e
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-11 13:28:44 +0530

    Create Inherit.java

---
+++ JavaProgram/src/Interface/inherit_interface/Inherit.java
@@ -0,0 +1,13 @@
+//Inherited class have to/must implement All method in Inheritance chain
+public class Inherit implements B {
+	public void method_3(){
+		System.out.println("Method 3");
+	}
+	public void method_2(){
+		System.out.println("Method 2");
+	}
+	public void method_1(){
+		System.out.println("Method 1");
+	}
+	
+}
