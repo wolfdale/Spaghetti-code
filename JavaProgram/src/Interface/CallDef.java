32b00b01579ff5ed71e68d076e8cee2499562cc7
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-11 12:50:26 +0530

    Create CallDef.java
    Definition

---
+++ JavaProgram/src/Interface/CallDef.java
@@ -0,0 +1,9 @@
+//Interface Method Defination always Public
+public class CallDef implements CallBack {
+	public void CallBack(int param){
+		System.out.println("Interface Demo "+param);
+		
+	}
+}
+
+
