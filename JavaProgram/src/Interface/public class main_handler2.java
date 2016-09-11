46fea44b3262ebd08b793a0f44e2686411fcc090
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-11 13:09:07 +0530

    Create public class main_handler2.java
    another method to call Interface Method

---
+++ JavaProgram/src/Interface/public class main_handler2.java
@@ -0,0 +1,11 @@
+
+public class main_handler2 {
+	public static void main(String [] args)
+	{
+		//As Concept :- a superclass reference to access a subclass object
+		//Same way for interface :- declare variables as object references 
+		//that use an interface rather than a class type
+		CallBack obj = new CallDef();
+		obj.CallBack(6);
+	}
+}
