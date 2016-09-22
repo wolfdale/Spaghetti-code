940d55f1371b2559ff991abda2f7c4317bd5996b
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-22 09:58:54 +0530

    Create DemoInput.java

---
+++ JavaProgram/src/DemoInput.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class DemoInput{
+	public static void main(String [] args)
+	{
+		//Reading console inputs (Deprecated way)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
