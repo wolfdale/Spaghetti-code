9fe607abcf0f5805ec6b9cec23f42072775174a4
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-23 10:19:47 +0530

    Create BRRead.java

---
+++ JavaProgram/src/BRRead.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class BRRead {
+ public static void main(String args[]) throws IOException
+ {
+ char c;
+ BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ do {
+ c = (char) br.read();
+ System.out.println(c);
+ } while(c != 'q');
+ }
+}
