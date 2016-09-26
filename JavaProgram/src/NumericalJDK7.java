507faee4800bb31d1c201eee16da4506221838f2
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-26 10:32:18 +0530

    Create NumericalJDK7.java

---
+++ JavaProgram/src/NumericalJDK7.java
@@ -0,0 +1,10 @@
+class NumericalJDK7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x = 10_000_000;
+		System.out.println(x);
+		double y = 2_3.12_34;
+		System.out.println(y);
+	}
+}
