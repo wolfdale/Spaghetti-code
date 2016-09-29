340ee131bbc700c66deeda670a4d17c70749f707
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-29 23:43:55 +0530

    Update q_to_stack.c

--- C_Prep/q_to_stack.c
+++ C_Prep/q_to_stack.c
@@ -2,26 +2,37 @@
 #include<stdlib.h>
 
 struct qnode* deqeue(struct qnode* head);
-void push(struct qnode* head,int x);
 struct qnode* pop(struct qnode* head);
+struct qnode* enqueue(struct qnode* head,int x);
+struct qnode* push(struct qnode* head,int x);
+void printer(struct qnode *head);
 
 struct qnode{
 	int data;
 	struct qnode* next;
 };
 
-void enqueue(struct qnode* head,int x)
+struct qnode* enqueue(struct qnode* head,int x)
 {
 	struct qnode* ptr = (struct qnode*)malloc(sizeof(struct qnode));
 	struct qnode* temp;
 	temp = head;
-	while(temp->next !=NULL)
-	{
-		temp = temp->next;
-	}
 	ptr->data = x;
 	ptr->next = NULL;
-	
+	if(temp == NULL)
+	{
+		temp = ptr;
+		return head;
+	}
+	else
+	{
+		while(temp->next !=NULL)
+		{
+			temp = temp->next;
+		}
+		temp = ptr;
+		return head;
+	}
 }
 
 struct qnode* deqeue(struct qnode* head)
@@ -29,10 +40,10 @@ struct qnode* deqeue(struct qnode* head)
 	return head->next;	
 }
 
-void push(struct qnode* head,int x)
+struct qnode* push(struct qnode* head,int x)
 {
-	enqueue(head,x);
-	
+	head = enqueue(head,x);
+	return head;
 }
 struct qnode* pop(struct qnode* head)
 {
@@ -40,9 +51,22 @@ struct qnode* pop(struct qnode* head)
 	return head;
 }
 
+void printer(struct qnode *head)
+{
+	struct qnode* temp = head;
+	while(temp){
+		printf("%d\n",temp->data);
+		temp = temp->next;
+
+	}
+
+}
 int main(void) {
 
 	struct qnode* head = NULL;
-	push(head,5);
-
+	head = push(head,5);
+	head = push(head,6);
+	head = push(head,7);
+	head = push(head,8);
+	printer(head);
 }
