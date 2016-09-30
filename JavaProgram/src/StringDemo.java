60d7252f3148b6eed0ec419733f6a6c4ccf388a1
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-30 09:39:42 +0530

    Create StringDemo.java

---
+++ JavaProgram/src/StringDemo.java
@@ -0,0 +1,18 @@
+//Strings Demo
+class StringDemo{
+	public static void main(String [] args)
+	{
+		char chr[] = {'j','a','v','a'};
+		
+		//Creating string and initilizing it in constructor.
+		String str = new String(chr);
+		System.out.println(str);
+		
+		//Creating string form particular index
+		String str1 = new String(chr,1,2);
+		System.out.println(str1);
+		
+		//java 
+		
+	}
+}
