cb77a4b9ab17ae0a2b07e228917a849c47f9a52a
Prakhar Gupta <mywolfdale@gmail.com>
2016-09-30 09:49:40 +0530

    Update StringDemo.java

--- JavaProgram/src/StringDemo.java
+++ JavaProgram/src/StringDemo.java
@@ -1,10 +1,10 @@
-//Strings Demo
 class StringDemo{
 	public static void main(String [] args)
 	{
 		char chr[] = {'j','a','v','a'};
 		
 		//Creating string and initilizing it in constructor.
+		//if we modify array the string will remain uncahnged
 		String str = new String(chr);
 		System.out.println(str);
 		
@@ -12,7 +12,18 @@ class StringDemo{
 		String str1 = new String(chr,1,2);
 		System.out.println(str1);
 		
-		//java 
+		//String length 
+		System.out.println(str1.length());
+		
+		//Java’s char type uses 16 bits to represent the basic Unicode character set, 
+		// Internet uses arrays of 8-bit bytes constructed from the
+		//ASCII character set
+		
+		byte char_set[] = {65,67,76,73,84,86};//Ascii values
+		String str2 = new String(char_set);
+		System.out.println(str2);
+		String str3 = new String(char_set,1,4);
+		System.out.println(str3);
 		
 	}
 }
