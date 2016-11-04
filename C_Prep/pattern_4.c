0a4b69c475eed3782c937570852248219eec7732
Prakhar Gupta <mywolfdale@gmail.com>
2016-11-04 10:07:18 +0530

    Create pattern_4.c

---
+++ C_Prep/pattern_4.c
@@ -0,0 +1,19 @@
+/*
+1234567
+12345
+123
+1
+*/
+#include<stdio.h>
+int main(){
+int i,j;
+for(i=7;i>=1;i-=2){
+        for(j=1;j<=i;j++){
+                printf("%d",j);
+
+        }
+        printf("\n");
+        }
+return 0;
+
+}
