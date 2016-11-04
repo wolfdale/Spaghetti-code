af1af1ae567aa23e6c2cd8a1c10368b095319ada
Prakhar Gupta <mywolfdale@gmail.com>
2016-11-04 10:00:34 +0530

    Create pattern_3.c

---
+++ C_Prep/pattern_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int i,j;
+for(i=1;i<=5;i++){
+        if(i%2!=0){
+                for(j=1;j<=6-i;j++){
+                printf("%d",j);
+                }
+        }
+        else{
+                for(j=6-i;j>=1;j--){
+                        printf("%d",j);
+
+                }
+        }
+        printf("\n");
+}
+return 0;
+
+}
