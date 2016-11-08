d258dc7a057a5a1ab01594334e44432e67a590a2
Prakhar Gupta <mywolfdale@gmail.com>
2016-11-08 11:10:37 +0530

    Create Pattern_14.c

---
+++ C_Prep/Pattern_14.c
@@ -0,0 +1,21 @@
+/*
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+
+#include<stdio.h>
+   int main(){
+       int i,j;
+       for(i=1;i<=4;i++){
+           for(j=1;j<=i;j++)
+           {
+               printf("%c",'A'+j-1);
+           }
+           printf("\n");
+      }
+      return 0;
+  }
