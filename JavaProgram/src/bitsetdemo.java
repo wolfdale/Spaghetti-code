2b1c72c150c9465eaffb119b88ea18a805804aa0
Prakhar <mywolfdale@gmail.com>
2016-11-11 20:26:39 +0530

    bit

---
+++ JavaProgram/src/bitsetdemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.BitSet;
+import java.util.ArrayList;
+class bitsetdemo{
+	public static void main(String [] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int tc = in.nextInt();
+		System.out.println(tc);
+		ArrayList<String> words;
+		
+		for(int i = 0;i<tc;i++)
+		{
+			String temp = in.nextline();
+			words.add(temp);
+		}
+
+	}
+}
+
+
+
+
