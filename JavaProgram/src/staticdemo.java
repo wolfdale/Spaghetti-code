cdc300d6c09012eb488c083fb18b879aedc749af
Prakhar Gupta <mywolfdale@gmail.com>
2016-11-18 12:34:16 +0530

    Create staticdemo.java

---
+++ JavaProgram/src/staticdemo.java
@@ -0,0 +1,10 @@
+class staticdemo{
+    //static block
+    static{
+        System.out.println("Static block");
+    }
+
+    public static void main(String [] args){
+        System.out.println("Main Method");
+    }
+}
