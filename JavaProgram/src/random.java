1d32522d6167b4f794f8cbf71833032c821ef3f5
Prakhar <mywolfdale@gmail.com>
2016-11-22 21:16:48 +0530

    Random

---
+++ JavaProgram/src/random.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+class getRandom{
+	public static void main(String [] args){
+		Random r = new Random();
+		// Random number under 50
+		System.out.println("Random :" + r.nextInt(50));
+	 }
+}
