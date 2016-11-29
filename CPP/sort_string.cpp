0d8b902a80c106fd88d6e2f12335e64a0cfa2542
Prakhar <mywolfdale@gmail.com>
2016-11-29 21:13:44 +0530

    sort

---
+++ CPP/sort_string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<ctype.h>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+int main(){
+	char str[100];
+	
+	string xx;
+	cin>>xx;	
+	//IN case you want to sort string
+	std::sort( xx.begin(), xx.end() );
+	cout<<xx;
+
+
+
+}
+
+
+
+
+
+
+
+
+
