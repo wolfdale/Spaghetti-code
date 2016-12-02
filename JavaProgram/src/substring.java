74c3071891b5a1bef9a61d085b442da5941e14b3
Prakhar <mywolfdale@gmail.com>
2016-12-02 21:23:05 +0530

    Substring check

---
+++ JavaProgram/src/substring.java
@@ -0,0 +1,9 @@
+class substring_check{
+	public static void main(String[] args){
+		String str1 = "abc";
+		String str2 = "okayabcdone";
+		boolean x = str2.contains(str1);
+		System.out.println(x);
+	}
+}
+		
