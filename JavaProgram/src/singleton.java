c9c89535a362c587076e7ecdb5a624214e7e073d
Prakhar <mywolfdale@gmail.com>
2017-02-11 20:20:48 +0530

    Comments

--- JavaProgram/src/singleton.java
+++ JavaProgram/src/singleton.java
@@ -1,10 +1,12 @@
 class Logging{
+	//Final Static private obj
 	private static final Logging SingleInstance = new Logging();
 
 	/*Private constructor*/
 	private Logging(){
 		System.out.println("Private Constructor");
 	}
+	//Accesor Method for private obj
 	public static Logging getSingleInstance(){
 		return SingleInstance;	
 	}
@@ -14,6 +16,7 @@ class Logging{
 }
 class runner{
 	public static void main(String[] args){
+		//Using directly class name
 		Logging.getSingleInstance().log("LOL");
 	}
 }
