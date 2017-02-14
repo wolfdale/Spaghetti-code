a625988776894e918545ad06c1ecf91b66bc0ac8
Prakhar <mywolfdale@gmail.com>
2017-02-14 22:21:01 +0530

    Trail

--- Testing/env/ref_global.c
+++ Testing/env/ref_global.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
+#include"runner.h"
 int caller(){
 	extern int DEBUG_ENV;
-	printf("DEBUG-->%d",DEBUG_ENV);
+	printf("DEBUG-->%d\n",DEBUG_ENV);
+	#ifdef DEBUG_MAC_ENV
+	printf("Printing Trace..");
+	#endif
 }
