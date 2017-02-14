0837807fc5bf37c9f0b38cfc213ba4a4dea0b104
Prakhar <mywolfdale@gmail.com>
2017-02-14 22:20:21 +0530

    Trial

--- Testing/env/read_env.c
+++ Testing/env/read_env.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include"runner.h"
 //Global
 int DEBUG_ENV = 0;
+	
 int main()
 {
     char* getEnv;
     char *env = "DEBUG";
     getEnv = getenv(env);
     DEBUG_ENV = atoi(getEnv);
+    #ifdef DEBUG_MAC_ENV
     printf("Value is = %d\n",DEBUG_ENV);
+    #endif
     caller();
 }
