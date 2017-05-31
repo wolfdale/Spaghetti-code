2dbcdd5ac9f62f5b75f5599dad4a662e05401b87
Prakhar Gupta <mywolfdale@gmail.com>
2017-05-31 21:23:34 +0530

    Create ThisWillCompile.java

---
+++ JavaProgram/src/Practice/ThisWillCompile.java
@@ -0,0 +1,8 @@
+class Curiosity {
+    public static void main (String[ ]  args)
+    { 
+        System.out.println ("A curiosity"); 
+        http: //kaddy.co   
+        System.out.println ("compile Ok!");
+    }
+}
