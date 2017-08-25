d3673a8908d40471113a0daaf79a30ebcddf86f8
Prakhar G Wolf <mywolfdale@gmail.com>
2017-08-25 11:23:56 +0530

    InComplete

---
+++ JavaProgram/src/StringDup.java
@@ -0,0 +1,28 @@
+package com.client.utils;
+import java.util.*;
+class FindDuplicate{
+	void getDuplicate(String ss){
+		HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
+		for(int i = 0 ;i<ss.length(); i++){
+			if(  ){
+			
+			}
+			else{
+				hash.put(ss.charAt(i), 1);
+			}
+
+
+	}
+
+	public static void main(String ... args){
+		getDuplicate("abcdefghijkabc");
+	}
+
+
+}
+
+
+
+
+
+
