ba014555981ccc9cd58007bd03e7bed31820c4a8
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-14 22:03:05 +0530

    Create runner.java

---
+++ Code08/Java/Java8DateTime/runner.java
@@ -0,0 +1,9 @@
+import java.time.*;
+class Runner{
+	public static void main(String[] args){
+        LocalDate d2 = LocalDate.of(2015, 2, 5);
+        LocalDate d3 = LocalDate.now();
+        System.out.println(d2);
+        System.out.println(d3);
+	}
+}
