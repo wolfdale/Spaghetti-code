a4cba4e2b5a91e5d5144d5005de29754382539fd
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-17 18:41:28 +0530

    Create digit.cpp

---
+++ CPP/digit.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+  
+using namespace std;
+ 
+int digSum(int n)
+{
+    int sum = 0;
+    
+    while(n > 0 || sum > 9)
+    {
+        if(n == 0)
+        {
+            n = sum;
+            sum = 0;
+        }
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n = 1234;
+    cout << digSum(n);
+    return 0;
+}
