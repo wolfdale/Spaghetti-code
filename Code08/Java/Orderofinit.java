d1650160fb17730a4508489f5a8a37b0ad05b61a
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-18 07:31:38 +0530

    Create Orderofinit.java

---
+++ Code08/Java/Orderofinit.java
@@ -0,0 +1,13 @@
+class A{
+   public A() { }
+   public A(int i) {   System.out.println(i );    }
+}
+class B{
+   static A s1 = new A(1);
+   A a = new A(2);
+   public static void main(String[] args){
+      B b = new B();
+      A a = new A(3);
+   }
+   static A s2 = new A(4);
+}
