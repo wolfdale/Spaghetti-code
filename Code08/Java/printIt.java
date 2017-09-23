88cec37d8bde90a8caa2c08d1edc2482bae958aa
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-23 23:22:37 +0530

    Create printIt.java

---
+++ Code08/Java/printIt.java
@@ -0,0 +1,7 @@
+class TestClass{
+      public static void main(String[] args) throws Exception{
+            int a = Integer.MIN_VALUE;
+            int b = -a;
+            System.out.println( a+ "   "+b);
+      }
+}
