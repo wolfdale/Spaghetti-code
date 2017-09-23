d69357676269b4de64a89fbc24c6932f865cb000
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-23 23:00:02 +0530

    Create orderOfExecution.java

---
+++ Code08/Java/orderOfExecution.java
@@ -0,0 +1,14 @@
+class A{
+   public A() { }
+   public A(int i) {   System.out.println(i );    }
+}
+//Static will run after class loading 
+class B{
+   static A s1 = new A(1);
+   A a = new A(2);
+   public static void main(String[] args){
+      B b = new B();
+      A a = new A(3);
+   }
+   static A s2 = new A(4);
+}
