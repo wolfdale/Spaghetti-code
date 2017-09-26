bc016162c52e25f051b089f3a167f7cfd878d5bd
Prakhar Gupta <mywolfdale@gmail.com>
2017-09-26 07:45:40 +0530

    Create removeDuplicateElements.c

---
+++ Code08/LL/removeDuplicateElements.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+	int data;
+	struct node* next;
+};
+void insert (struct node**ref,int item){
+	struct node* newnode = (struct node*)malloc(sizeof(struct node));
+	if(*ref == NULL){
+		*ref = newnode;
+		newnode->data=item;
+	}
+	else{
+		newnode->next = *ref;
+		*ref = newnode;
+		newnode->data = item;
+	}
+	
+}
+
+void removedup(struct node** ref){
+
+	struct node* curr = *ref;
+	struct node* temp;
+	while(curr->next !=NULL){
+		if(curr->data == curr->next->data){
+			temp = curr->next->next;
+			free(curr->next);
+			curr->next = temp;
+		}
+		else{
+			curr = curr->next;
+		}
+	}
+}	
+void printer(struct node* ref){
+	
+		struct node* temp = ref;
+		
+		while(temp != NULL){
+			printf("%d->",temp->data);
+			temp = temp->next;
+		}
+		printf("\n");
+	
+}
+
+int main(void) {
+	struct node* head = NULL;
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	insert(&head,1);
+	
+	insert(&head,2);
+	insert(&head,3);
+	insert(&head,4);
+	insert(&head,5);
+	insert(&head,6);
+	insert(&head,7);
+	insert(&head,7);
+	
+	printer(head);
+	removedup(&head);
+	printer(head);
+	return 0;
+}
