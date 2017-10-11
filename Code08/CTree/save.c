61347dc0c3d45d5080555c254b1877a88bcbd2d1
Prakhar Gupta <mywolfdale@gmail.com>
2017-10-11 22:56:15 +0530

    Update save.c

--- Code08/CTree/save.c
+++ Code08/CTree/save.c
@@ -1,18 +1,13 @@
-// C program for different tree traversals
+
 #include <stdio.h>
 #include <stdlib.h>
- 
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
+
 struct node
 {
      int data;
      struct node* left;
      struct node* right;
 };
- 
-/* Helper function that allocates a new node with the
-   given data and NULL left and right pointers. */
 struct node* newNode(int data)
 {
      struct node* node = (struct node*)
@@ -25,7 +20,6 @@ struct node* newNode(int data)
 }
  
 
-/* Given a binary tree, print its nodes in inorder*/
 int printInorder(struct node* node,int a[])
 {
 	static int x=0;
