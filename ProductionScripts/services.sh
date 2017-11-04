bacecca94ce11c4291f9edbeb99d2909ce443c20
Prakhar Gupta <mywolfdale@gmail.com>
2017-11-04 21:37:40 +0530

    Create services.sh

---
+++ ProductionScripts/services.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+echo "***************************************************"
+echo $now
+echo "***************************************************"
+#nginx status
+sudo systemctl status nginx | tee ../log/Log_$now
+#Redis Db Status
+sudo systemctl status redis | tee -a ../log/Log_$now
+#PM2 NodeJS Status
+pm2 show 0 | tee -a ../log/Log_$now
