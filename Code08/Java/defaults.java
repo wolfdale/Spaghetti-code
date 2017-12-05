c6b0c3bdc828caf059253cb957c59ecde2b94c7e
Prakhar Gupta <mywolfdale@gmail.com>
2017-12-05 08:30:59 +0530

    Create defaults.java

---
+++ Code08/Java/defaults.java
@@ -0,0 +1,9 @@
+//Output - null
+class TestClass{
+    public static void main(String[] args){
+        int index = 1; 
+        String[] strArr = new String[5]; 
+        String   myStr  = strArr[index]; 
+        System.out.println(myStr);
+    }
+}
