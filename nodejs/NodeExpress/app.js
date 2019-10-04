414d71313e2bbf86b7f1efb83353d362448bee32
Prakhar Gupta <mywolfdale@gmail.com>
2019-10-04 14:55:54 +0530

    node js clean up

--- nodejs/NodeExpress/app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(request, response){
-	response.json({ 'Message':'This is from Express' })
-	});
-app.listen(process.env.PORT|8080);
--- nodejs/templateEngine/routes/index.js
+++
@@ -1,5 +0,0 @@
-//INdex Page Rendering
-
-exports.index = function(request, response){
-	response.render('index',{title:'Hey !',message: 'Im Pug'});
-};
