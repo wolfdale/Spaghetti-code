c182c6e8155c6980454ef3b2f3705788d590c30c
Prakhar Gupta <mywolfdale@gmail.com>
2019-10-08 20:24:55 +0530

    Temp

--- Golang/Pointers/GoTreeHeight.go
+++ Golang/Pointers/GoTreeHeight.go
@@ -1,37 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import ("fmt"
+		"GoTreeDs")
 
-type TreeNode struct {
-	data  int64
-	left  *TreeNode
-	right *TreeNode
-}
 
 func main() {
-	root := &TreeNode{65, nil, nil}
-	root.left = &TreeNode{85, nil, nil}
-	root.right = &TreeNode{95, nil, nil}
-	root.right.right = &TreeNode{95, nil, nil}
-	root.right.right.right = &TreeNode{95, nil, nil}
+	root := &treeNode{65, nil, nil}
+	root.left = &treeNode{85, nil, nil}
+	root.right = &treeNode{95, nil, nil}
+	//root.right.right = &treeNode{95, nil, nil}
+	//root.right.right.right = &treeNode{95, nil, nil}treeNode
 
-	fmt.Print(heightOfTree(root))
+	fmt.Print(HeightOfTree(root))
 
-}
-
-func heightOfTree(root *TreeNode) int64 {
-	if root == nil {
-		return 0
-	}
-
-	leftSide := heightOfTree(root.left)
-	rightSide := heightOfTree(root.right)
-
-	if leftSide > rightSide {
-		return 1 + leftSide
-	} else {
-		return 1 + rightSide
-	}
 }
