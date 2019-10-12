2c653cd42c353d9cb631822eb6be1ff4a8f5bc4e
Prakhar Gupta <prakgupt@redhat.com>
2019-10-12 20:10:55 +0530

    Revert "Tree Height in GoLang (#24)"
    This reverts commit d1985323c4d016cb2e2052a6d2f458d105edee5f.

--- Golang/Pointers/TreeTraversal.go
+++ Golang/Pointers/TreeTraversal.go
@@ -2,17 +2,52 @@ package main
 
 import "fmt"
 
+type Node struct {
+	val   int64
+	left  *Node
+	right *Node
+}
+
+func InOrderTraversal(root *Node) {
+	if root == nil {
+		return
+	}
+
+	InOrderTraversal(root.left)
+	fmt.Printf("%d ", root.val)
+	InOrderTraversal(root.right)
+}
+
+func PreOrderTraversal(root *Node) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.val)
+	PreOrderTraversal(root.left)
+	PreOrderTraversal(root.right)
+}
+
+func PostOrderTraversal(root *Node) {
+	if root == nil {
+		return
+	}
+
+	PostOrderTraversal(root.left)
+	PostOrderTraversal(root.right)
+	fmt.Printf("%d ", root.val)
+}
+
 func main() {
-	root := &treeNode{10, nil, nil}
-	root.left = &treeNode{20, nil, nil}
-	root.right = &treeNode{30, nil, nil}
-	root.left.left = &treeNode{40, nil, nil}
-	root.left.right = &treeNode{50, nil, nil}
-	root.right.right = &treeNode{60, nil, nil}
-	root.left.left.left = &treeNode{70, nil, nil}
+	root := &Node{10, nil, nil}
+	root.left = &Node{20, nil, nil}
+	root.right = &Node{30, nil, nil}
+	root.left.left = &Node{40, nil, nil}
+	root.left.right = &Node{50, nil, nil}
+	root.right.right = &Node{60, nil, nil}
+	root.left.left.left = &Node{70, nil, nil}
 
 	fmt.Println("In Order Traversal")
-	inOrderTraversal(root)
+	InOrderTraversal(root)
 	fmt.Println("\nPre Order Traversal")
 	PreOrderTraversal(root)
 	fmt.Println("\nPost Order Traversal")
